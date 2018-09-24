6e064b2be0668940dd483144fc504cdd267fe769
Veniel Dorestave <veniel@noukod.com>
2018-09-24 17:53:32 +0000

    automatically backed up by learn

--- lib/meowing_cat.rb
+++ lib/meowing_cat.rb
@@ -1,11 +1,11 @@
-## code your solution here. 
-class Cat 
+## code your solution here.
+class Cat
 attr_reader: name
 attr_writer: name
 
 maru = Cat.new
 maru.name = "Maru"
- 
+
 maru.name
 maru.meow
 def meow
